a40082a1a935450b19523b381263f0f9bd4e1c87
rabbitnoi <chayanit@sirisoft.co.th>
2019-11-12 23:41:00 -0800

    please work

--- create_database.sql
+++ create_database.sql
@@ -3,16 +3,16 @@ GO
 
 
 
-CREATE DATABASE AdventureWorks
+CREATE DATABASE AdventureWorks_1
 ON  (NAME = Sales_dat,
-    FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATAsaledat.mdf',
+    FILENAME = 'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATAsaledat_1.mdf',
     SIZE = 10,
     MAXSIZE = 50,
     FILEGROWTH = 5 )
 
 LOG ON
 ( NAME = Sales_log,
-    FILENAME =   'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATAsalelog.ldf',
+    FILENAME =   'C:\Program Files\Microsoft SQL Server\MSSQL14.MSSQLSERVER\MSSQL\DATAsalelog_1.ldf',
     SIZE = 5MB,
     MAXSIZE = 25MB,
     FILEGROWTH = 5MB ) ;
